cd6169446a19ead670583a6de6bec5b1c8535528
Tom Rolandus <tomrolandus@Toms-MacBook-Pro-6.local>
2016-10-20 22:26:55 +0800

    Starting

--- app/src/main/java/sg/edu/smu/livelabs/citygangs/MainActivity.java
+++ app/src/main/java/sg/edu/smu/livelabs/citygangs/MainActivity.java
@@ -2,8 +2,11 @@ package sg.edu.smu.livelabs.citygangs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String msg = "Android : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //just to check
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
 }
