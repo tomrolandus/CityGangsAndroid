89eeea51f80e0d8f54d92dd3eb255424b5c12c5b
Tom Rolandus <tomrolandus@Toms-MacBook-Pro-6.local>
2016-11-13 19:20:39 +0800

    forgot unstaged files

--- app/src/main/java/sg/edu/smu/livelabs/citygangs/FaceAPI/ui/SelectImageActivity.java
+++ app/src/main/java/sg/edu/smu/livelabs/citygangs/FaceAPI/ui/SelectImageActivity.java
@@ -32,13 +32,16 @@
 //
 package sg.edu.smu.livelabs.citygangs.FaceAPI.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -47,6 +50,7 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.IOException;
 
+
 import sg.edu.smu.livelabs.citygangs.R;
 
 // The activity for the user to select a image and to detect faces in the image.
@@ -54,6 +58,7 @@ public class SelectImageActivity extends AppCompatActivity {
     // Flag to indicate the request of the next task to be performed
     private static final int REQUEST_TAKE_PHOTO = 0;
     private static final int REQUEST_SELECT_IMAGE_IN_ALBUM = 1;
+    private static int MY_REQUEST_CODE;
 
     // The URI of photo taken with camera
     private Uri mUriPhotoTaken;
@@ -62,6 +67,7 @@ public class SelectImageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("DEBUG","SelectImageActivity_OnCreate");
         setContentView(R.layout.activity_select_image);
     }
 
@@ -104,21 +110,61 @@ public class SelectImageActivity extends AppCompatActivity {
         }
     }
 
-    // When the button of "Take a Photo with Camera" is pressed.
-    public void takePhoto(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            // Save the photo taken to a temporary file.
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            try {
-                File file = File.createTempFile("IMG_", ".jpg", storageDir);
-                mUriPhotoTaken = Uri.fromFile(file);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
-                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
-            } catch (IOException e) {
-                setInfo(e.getMessage());
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debugPermission", "PERMISSION_GRANTED");
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    // Save the photo taken to a temporary file.
+                    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    try {
+                        File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                        mUriPhotoTaken = Uri.fromFile(file);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
+                        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+                    } catch (IOException e) {
+                        setInfo(e.getMessage());
+                    }
+                }
+            }
+            else {
+                Log.d("debugPermission", "PERMISSION_DENIED");
+                // Your app will not have this permission. Turn off all functions
+                // that require this permission or it will force close like your
+                // original question
             }
         }
+    }
+
+    // When the button of "Take a Photo with Camera" is pressed.
+    public void takePhoto(View view) {
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("debugPermission", "checkSelfPermission");
+
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    MY_REQUEST_CODE);
+        }
+        else{
+            Log.d("debugPermission","checkSeflPermission else");
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                // Save the photo taken to a temporary file.
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                try {
+                    File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                    mUriPhotoTaken = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
+                    startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+                } catch (IOException e) {
+                    setInfo(e.getMessage());
+                }
+            }
+        }
+
     }
 
     // When the button of "Select a Photo in Album" is pressed.
